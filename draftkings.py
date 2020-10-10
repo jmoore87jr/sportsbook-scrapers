5ea1a720d8aa1e782b4e8752981f8b429f0459b3
jmoore87jr <base_1187@yahoo.com>
2020-10-09 17:41:27 -0700

    fixed multiple tables on DK site and try/except for incomplete table

--- draftkings.py
+++ draftkings.py
@@ -6,51 +6,71 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import logging
 import time 
+import sqlite3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+def save_to_SQLite():
+    pass
+
 def scrape_nfl_gamelines():
-    logger.info("Retrieving DraftKings Game Lines")
+    logger.info("Retrieving DraftKings game lines...")
     # create soup
     r = requests.get("https://sportsbook.draftkings.com/leagues/football/3?category=game-lines&subcategory=game")
     src = r.content 
     soup = BeautifulSoup(src, 'lxml')
 
-    # divide into 4 columns
-    sbtable = soup.find('div', {'class': 'sportsbook-table__body'}) # look at the class containing the betting info
-    cols = [ child for child in sbtable.children ] # teams, spread, total, moneyline
-    teams = cols[0]
-    spreads = cols[1]
-    totals = cols[2]
-    moneylines = cols[3]
+    # iterate over every table of games on the web page
+    tables = soup.findAll('div', {'class': 'sportsbook-table__body'})
+    dfs = []
+    errors = []
+    for table in tables:
+        # divide table into 4 columns
+        #table = soup.find('div', {'class': 'sportsbook-table__body'}) # look at the class containing the betting info
+        cols = [ child for child in table.children ] # teams, spread, total, moneyline
+        teams = cols[0]
+        spreads = cols[1]
+        totals = cols[2]
+        moneylines = cols[3]
 
-    # team names
-    team_names = [ node.text for node in teams.findAll('span', {'class': 'event-cell__name'}) ]
-    # spreads
-    spread_nums = [ node.text for node in spreads.findAll('span', {'class': 'sportsbook-outcome-cell__line'}) ]
-    # spread price
-    spread_prices = [ node.text for node in spreads.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
-    # totals
-    total_nums = [ node.text for node in totals.findAll('span', {'class': 'sportsbook-outcome-cell__line'}) ]
-    # totals price
-    total_prices = [ node.text for node in totals.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
-    # moneyline
-    moneyline_nums = [ node.text for node in moneylines.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
+        # team names
+        team_names = [ node.text for node in teams.findAll('span', {'class': 'event-cell__name'}) ]
+        # spreads
+        spread_nums = [ node.text for node in spreads.findAll('span', {'class': 'sportsbook-outcome-cell__line'}) ]
+        # spread price
+        spread_prices = [ node.text for node in spreads.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
+        # totals
+        total_nums = [ node.text for node in totals.findAll('span', {'class': 'sportsbook-outcome-cell__line'}) ]
+        # totals price
+        total_prices = [ node.text for node in totals.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
+        # moneyline
+        moneyline = [ node.text for node in moneylines.findAll('span', {'class': 'sportsbook-odds american default-color'}) ]
 
-    df_gamelines = pd.DataFrame( 
-                {'Team': team_names,
-                'Spread': spread_nums,
-                'Spread_Price': spread_prices,
-                'Total': total_nums,
-                'Total_Price': total_prices,
-                'Moneyline': moneyline_nums})
+        try:
+            df = pd.DataFrame( 
+                        {'Team': team_names,
+                        'Spread': spread_nums,
+                        'Spread_Price': spread_prices,
+                        'Total': total_nums,
+                        'Total_Price': total_prices,
+                        'Moneyline': moneyline})
+        except ValueError:
+            for i in range(0, len(team_names), 2):
+                errors.append(f"Error: Bets haven't been posted for {team_names[i]} vs. {team_names[i+1]}")
+            continue
 
-    print(df_gamelines)
+        dfs.append(df)
+    
+    result = pd.concat(dfs)
+    print(result)
 
-    # store in SQLite table
+    for error in errors:
+        print(error)
 
-    return df_gamelines
+    # TODO: store in SQLite table
+
+    return result
 
 def scrape_nfl_player_props():
     pass 
@@ -60,9 +80,10 @@ def scrape_nba_player_props():
 
 def main():
     while True:
+        wait_time = 30
         scrape_nfl_gamelines()
-        logger.info("Waiting...")
-        time.sleep(30)
+        logger.info(f"Waiting {wait_time}s...")
+        time.sleep(wait_time)
 
 if __name__ == "__main__":
     main()
